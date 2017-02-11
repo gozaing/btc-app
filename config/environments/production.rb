1be104584de1fc20cd6a19ff902984e5708b89d4
gozaing <steganographi@gmail.com>
2017-02-12 02:50:28 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.autoload_paths += Dir["#{config.root}/lib"]
-
 end
