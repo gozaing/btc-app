a962a48535adddb6b9f0d6ca7352d2ecce76ab0a
gozaing <steganographi@gmail.com>
2017-02-18 15:42:05 +0900

    change batch

--- lib/tasks/batch.rb
+++ lib/tasks/batch.rb
@@ -1,6 +1,32 @@
 class Tasks::Batch
   def self.execute
-    puts "Hello, logs"
+    require 'net/http'
+    require 'uri'
+    require 'json'
+
+    #uri = URI.parse(Settings.bitflyer[:api_path])
+    uri = URI.parse('https://api.bitflyer.jp')
+    #uri.path = '/v1/board'
+    uri.path = '/v1/ticker'
+    uri.query = ''
+
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    response = https.get uri.request_uri
+    result = JSON.parse(response.body)
+
+    ticker = Ticker.new
+    ticker.tick_id              = result['tick_id']
+    ticker.best_bid             = result['best_bid']
+    ticker.best_ask             = result['best_ask']
+    ticker.best_bid_size        = result['best_bid_size']
+    ticker.best_ask_size        = result['best_ask_size']
+    ticker.total_bid_depth      = result['total_bid_depth']
+    ticker.total_ask_depth      = result['total_ask_depth']
+    ticker.ltp                  = result['ltp']
+    ticker.volume               = result['volume']
+    ticker.volume_by_product    = result['volume_by_product']
+    ticker.save
 
   end
 end
