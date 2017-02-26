4f7e7eb629ff7a9fe165c8ac4f5f5eed8135a68c
gozaing <steganographi@gmail.com>
2017-02-26 22:52:41 +0900

    add get parent_order_id

--- lib/tasks/trade.rb
+++ lib/tasks/trade.rb
@@ -18,11 +18,6 @@ class Tasks::Trade
     ltp_low  = (last_ltp - (last_ltp * 0.01)).floor
     ltp_sell = (ltp_low  - (ltp_low  * 0.01)).floor
 
-    timestamp = Time.now.to_i.to_s
-    method = "POST"
-    uri = URI.parse("https://api.bitflyer.jp")
-    uri.path = "/v1/me/sendparentorder"
-
     body = '{
       "order_method": "IFDOCO",
       "time_in_force": "GTC",
@@ -64,11 +59,49 @@ class Tasks::Trade
     https.use_ssl = true
     response = https.request(options)
 
+    # Todo: use json parse
     str = response.body.match("parent_order_acceptance_id\":\"(.+)\"}")
 
+    parent_id = self.get_parent(str[1])
+
     p = Parent.new
+    p.parent_order_id            = parent_id
     p.parent_order_acceptance_id = str[1]
     p.save
+
+  end
+
+  def self.get_parent(accept_id)
+
+    require "net/http"
+    require "uri"
+    require "openssl"
+    require "json"
+
+    key    = ENV['BITFLYER_API_KEY']
+    secret = ENV['BITFLYER_API_TOKEN']
+
+    timestamp = Time.now.to_i.to_s
+    method = "GET"
+    uri = URI.parse(ENV['BITFLYER_API_URI'])
+    uri.path = "/v1/me/getparentorder"
+    uri.query = "parent_order_acceptance_id=" + accept_id
+
+    text = timestamp + method + uri.request_uri
+    sign = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), secret, text)
+
+    options = Net::HTTP::Get.new(uri.request_uri, initheader = {
+      "ACCESS-KEY" => key,
+      "ACCESS-TIMESTAMP" => timestamp,
+      "ACCESS-SIGN" => sign,
+    });
+
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    response = https.request(options)
+
+    result = JSON.parse(response.body)
+    result["parent_order_id"]
 
   end
 end
